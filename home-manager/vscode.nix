e946a7ee2e60d2415c5825227cfd5657c5ca483d
Pat <pat@patsullivan.org>
2026-03-16 11:05:25 -0400

    revert: vscode version change

--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -5,7 +5,7 @@
 }:
 
 let
-  inherit (pkgs.vscode-utils) buildVscodeMarketplaceExtension;
+  inherit (pkgsUnstable.vscode-utils) buildVscodeMarketplaceExtension;
 
   extension =
     {
@@ -27,13 +27,13 @@ let
 in
 {
   enable = true;
-  package = pkgs.vscode;
+  package = pkgsUnstable.vscode;
   mutableExtensionsDir = true;
   profiles = {
     default = {
       enableUpdateCheck = false;
       enableExtensionUpdateCheck = false;
-      extensions = with pkgs.vscode-extensions; [
+      extensions = with pkgsUnstable.vscode-extensions; [
         # Provided by Nixpkgs
         bbenoist.nix
         bierner.markdown-mermaid
