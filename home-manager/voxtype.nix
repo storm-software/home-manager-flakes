03b1a0628e7bea72d5874a6693705feb2e4a81f6
sullivanpj <pat@patsullivan.org>
2026-07-04 17:33:50 -0400

    chore: Updated services config

--- home-manager/voxtype.nix
+++ home-manager/voxtype.nix
@@ -7,17 +7,6 @@
     "tiny.en"
   ];
   settings = {
-    #     "model.base.en" = {
-    #       "voice" = "en_us_001";
-    #       "speed" = 1.0;
-    #       "pitch" = 1.0;
-    #       "volume" = 1.0;
-    #       "language" = "en-US";
-    #       "voice_id" = "en_us_001";
-    #       "voice_name" = "en_us_001";
-    #       "voice_gender" = "male";
-    #     };
-
     output = {
       fallback_to_clipboard = true;
       mode = "type";
