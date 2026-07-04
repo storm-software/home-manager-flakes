23fbde7cfe8fd825005ce70f5632c1464b6853fd
sullivanpj <pat@patsullivan.org>
2026-07-04 13:13:28 -0400

    fix: Resolve issues with invalid pkgs reference

--- home-manager/kodi.nix
+++ home-manager/kodi.nix
@@ -2,7 +2,7 @@
 
 {
   enable = true;
-  package = pkgs.kodi-wayland;
+  package = pkgs.stable.kodi-wayland;
   datadir = "${user.system.homeDirectory}/.kodi";
 
   # package = pkgs.kodi.withPackages (exts: [ exts.pvr-iptvsimple ]);
